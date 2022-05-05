5ec4a15c1bf3ad84619e92b0b4e3c42517266211
Admin <prasad.parshram123@gmail.com>
2022-05-05 15:55:53 +0530

    added home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Note app")),
+      body: SafeArea(
+          child: Center(
+        child: Text("Note app"),
+      )),
+    );
+  }
+}
