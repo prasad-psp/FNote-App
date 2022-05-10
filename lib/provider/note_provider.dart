f3c07764f6042275910a7b06216420ae4621e86b
Admin <prasad.parshram123@gmail.com>
2022-05-10 20:33:02 +0530

    added new Note provider

---
+++ lib/provider/note_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fnote_app/db/note_db.dart';
+
+import '../models/note_model.dart';
+
+class NoteProvider with ChangeNotifier {
+  NoteDatabase? noteDatabase;
+
+  List<NoteModel>? modelList;
+
+  void init() {
+    noteDatabase = NoteDatabase();
+    read();
+  }
+
+  void read() async {
+    List<NoteModel>? tempList = await noteDatabase?.read();
+    if (tempList != null) {
+      if (tempList.isNotEmpty) {
+        modelList = tempList;
+        notifyListeners();
+      }
+    }
+  }
+
+  void insert(NoteModel model, bool isDuplicate) async {
+    var noteId = await noteDatabase?.insert(model);
+    if (noteId != 0) {
+      read();
+    }
+  }
+}
